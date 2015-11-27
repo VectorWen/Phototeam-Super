df2a6dec8623314595389fe27834ded47475a7d1
VectorHuang <642378415@qq.com>
2015-11-27 17:51:22 +0800

    初始化，Teamleader 开始招人

---
+++ PhototeamSuper/src/main/java/com/vector/photo/team/niuer/PhoneLoader.java
@@ -0,0 +1,15 @@
+package com.vector.photo.team.niuer;
+
+/**
+ *
+ * 牛人之<b>Android系统数据库数据供应者</b>，专注读取数据库数据100年<p>
+ *
+ * Author: vector.huang
+ * Email: 642378415@qq.com
+ * Date: 2015/11/27
+ * Description:<p>{TODO: 用一句话描述}
+ */
+public class PhoneLoader {
+
+
+}
---
+++ PhototeamSuper/src/main/java/com/vector/photo/team/niuer/SDCardLoader.java
@@ -0,0 +1,10 @@
+package com.vector.photo.team.niuer;
+
+/**
+ * Author: vector.huang
+ * Email: 642378415@qq.com
+ * Date: 2015/11/27
+ * Description:<p>{TODO: 用一句话描述}
+ */
+public class SDCardLoader {
+}
